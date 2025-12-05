2f36bf1151a6891d725c2ee5910d48c651520f4e
Javier Espinoza R. <Javier.espinoza1989@gmail.com>
2025-12-05 12:59:11 -0300

    metodos de validacion utils

--- src/utils/request-helpers.ts
+++ src/utils/request-helpers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export class RequestHelpers {
-    
+
   static getIdParam(req: Request, res: Response): number | null {
     const idParam = req.params.id;
 
@@ -19,4 +19,37 @@ export class RequestHelpers {
 
     return id;
   }
+
+  static getBodyField<T>(req: Request, res: Response, field: string): T | null {
+    const value = req.body?.[field];
+
+    if (value === undefined || value === null) {
+      res.status(400).json({ message: `El campo '${field}' es requerido.` });
+      return null;
+    }
+
+    return value as T;
+  }
+
+  static validateNumericQueryParam(req: Request, res: Response, key: string): number | null {
+    const value = req.query?.[key];
+
+    if (!value) {
+      res.status(400).json({ message: `El parámetro '${key}' es requerido.` });
+      return null;
+    }
+
+    const num = Number(value);
+    if (Number.isNaN(num)) {
+      res.status(400).json({ message: `El parámetro '${key}' debe ser numérico.` });
+      return null;
+    }
+
+    return num;
+  }
+
+  static sendError(res: Response, status: number, message: string): void {
+    res.status(status).json({ message });
+  }
+
 }
